aa859a70e94ad3424809e08c9d9ad477eb398e59
Oasiris <teracontinens@gmail.com>
2019-12-16 03:06:36 -0800

    Small additions

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,14 @@ import App from './App'
 import './style/lib/bootstrap-grid.min.css'
 import './style/lib/normalize.css'
 import './index.css'
+import ErrorBoundary from './components/ErrorBoundary'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <ErrorBoundary>
+        <App />
+    </ErrorBoundary>,
+    document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
