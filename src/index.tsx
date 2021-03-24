b071f69ef0d7c46965b33b13ca9ab6e6b8c1102c
Oasiris <teracontinens@gmail.com>
2021-03-24 01:51:02 -0400

    Package refresh

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import * as serviceWorker from './utils/serviceWorker'
+import { ErrorBoundary } from './components/ErrorBoundary'
 
 import App from './App'
 
-// tslint:dsaible ordered-imports
+// tslint:disable ordered-imports
 import './styles/lib/bootstrap-grid.min.css'
 import './styles/lib/normalize.css'
 import './styles/index.scss'
-import { ErrorBoundary } from './components/ErrorBoundary'
 
 ReactDOM.render(
     <ErrorBoundary>
@@ -17,8 +16,3 @@ ReactDOM.render(
     </ErrorBoundary>,
     document.getElementById('root'),
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
--- tslint.js
+++ tslint.js
@@ -1,9 +1,8 @@
-// Exported as a JS file instead of a JSON file so comments do not show up as format errors on GitHub.
 module.exports = {
   extends: [
     './tslint-base.js',
 
-    // Disable formatting rules in the linter that might conflict with Prettier, our new formatter.
+    // Overwrite rules that are enforced by our formatter, Prettier.
     'tslint-react',
     'tslint-config-prettier',
 
