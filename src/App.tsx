1c4cc6e3e3c1127006ab645f7a1c20141e269ad4
David Hong <11142550+Oasiris@users.noreply.github.com>
2024-08-31 18:43:25 -0700

    Revert "Revert "refactor(all): update for 2024 (vite, react 18, node 20)""

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,110 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {
+    Route,
+    Link,
+    Outlet,
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+} from 'react-router-dom'
 
-import { Home } from './routes/Home'
+import Home from './routes/home'
+import ErrorBoundary from './routes/error-boundary'
 
-import { LoremIpsum } from './components/LoremIpsum'
-import { NotFound } from './components/NotFound'
+// Uncomment this when you're ready to replace "Not found" error boundary page with a dedicated "Not found" page.
+// import NoMatch from './routes/no-match'
 
-const Routes: React.FC = () => {
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Layout />} errorElement={<ErrorBoundary />}>
+            <Route errorElement={<ErrorBoundary />}>
+                <Route index element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="dashboard" element={<Dashboard />} />
+
+                {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+                {/* <Route path="*" element={<NoMatch />} /> */}
+            </Route>
+        </Route>,
+    ),
+)
+
+export default function App() {
     return (
-        <>
-            <Switch>
-                <Route path="/foobar" component={LoremIpsum} />
-                <Route path="/" exact component={Home} />
-                <Route path="/" component={NotFound} />
-            </Switch>
-        </>
+        <div>
+            <h3>
+                Basic <code>react-router</code> Layout
+            </h3>
+
+            <p>
+                This demonstrates some of the core features of React Router including nested{' '}
+                <code>&lt;Route&gt;</code>s, <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>
+                s, and using a "*" route (aka "splat route") to render a "not found" page when
+                someone visits an unrecognized URL.
+            </p>
+
+            {/* Routes nest inside one another. Nested route paths build upon
+                parent route paths, and nested route elements render inside
+                parent route elements. See the note about <Outlet> below. */}
+            <RouterProvider router={router} />
+        </div>
     )
 }
 
-const App: React.FC = () => {
+function Layout() {
     return (
-        <>
-            <Router>
-                <Routes />
-            </Router>
-        </>
+        <div>
+            {/* A "layout route" is a good place to put markup you want to
+            share across all the pages on your site, like navigation. */}
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link to="/nothing-here">Nothing Here</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <hr />
+
+            {/* An <Outlet> renders whatever child route is currently active,
+            so you can think about this <Outlet> as a placeholder for
+            the child routes we defined above. */}
+            <Outlet />
+
+            <hr />
+
+            <footer>
+                <br />
+                This is a basic footer.
+            </footer>
+        </div>
     )
 }
 
-export default App
+function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is the About section</p>
+        </div>
+    )
+}
+
+function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>This is the Dashboard</p>
+        </div>
+    )
+}
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,23 +1,46 @@
-import React from 'react'
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '../assets/vite.svg'
+import crabLogo from '/crab-favicon.svg'
+import './home.scss'
 
-import logo from '../icons/logo.svg'
+function Home() {
+    const [count, setCount] = useState(0)
 
-import '../styles/Home.css'
-
-export const Home: React.FC = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit
-                    <code>src/App.tsx</code>
-                    and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
-            </header>
-        </div>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={crabLogo} className="logo crab" alt="Crab logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <p>
+                    Edit <code>src/routes/Home.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+
+            <button
+                style={{ background: 'pink' }}
+                onClick={() => {
+                    throw new Error(
+                        'this is a custom error thrown at ' + new Date().toLocaleString(),
+                    )
+                }}
+            >
+                Click me to throw an error
+            </button>
+        </>
     )
 }
+
+export default Home
