f95534e2008198c01ffa9e949cb681ee192a0ae9
Oasiris <11142550+Oasiris@users.noreply.github.com>
2024-08-31 18:22:44 -0700

    react router boilerplate begin

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './routes/Home.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import Home from './routes/Home.tsx'
+import Root from './routes/root.tsx'
 import './styles/index.css'
+import ErrorPage from './routes/error-page.tsx'
+
+/*
+HELPFUL LINKS:
+- How to use JSX Routes in react-router: https://reactrouter.com/en/main/start/tutorial#the-contact-route-ui
+*/
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <ErrorPage />,
+    },
+])
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Home />
+        {/* <Home /> */}
+        <RouterProvider router={router} />
     </StrictMode>,
 )
